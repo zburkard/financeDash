176ff1f1554e95589cd24cdd5744c88bc39fcf81
zburkard <burkard.exe@gmail.com>
2023-03-27 14:50:23 -0600

    mui/vite/theme setup

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import { CssBaseline } from "@mui/material"
+import { ThemeProvider } from "@mui/material"
+import { createTheme } from "@mui/material/styles"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+
+
+function App() {
+  const theme = useMemo(()=> createTheme(themeSettings), [])
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+      </ThemeProvider>
+    </div>
+  )
+}
+
+export default App
