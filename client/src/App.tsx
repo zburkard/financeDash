bb1c5596260528d242245bda025959bbb90b61ea
zburkard <burkard.exe@gmail.com>
2023-03-28 15:06:24 -0600

    box and router set up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
+import { Box } from "@mui/material"
 import { CssBaseline } from "@mui/material"
 import { ThemeProvider } from "@mui/material"
 import { createTheme } from "@mui/material/styles"
 import { useMemo } from "react"
+import { BrowserRouter } from "react-router-dom"
 import { themeSettings } from "./theme"
 
 
@@ -9,9 +11,14 @@ function App() {
   const theme = useMemo(()=> createTheme(themeSettings), [])
   return (
     <div className="app">
+      <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline/>
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          
+        </Box>
       </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
